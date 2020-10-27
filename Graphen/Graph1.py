b6baaee18cd949df38d1d2f94b20c93261679895
Niklas Düser <niklas.dueser@tu-dortmund.de>
2020-10-27 23:14:18 +0100

    x und y Achse des Graphen getauscht, erste Version der Pdf 1) im Grunde vollständig.

--- Graphen/Graph1.py
+++ Graphen/Graph1.py
@@ -14,24 +14,24 @@ x=np.array([1.6,2.7,3.2,3.5,4.])
 def func(m,a,b):
     return a*m+b
 
-plt.plot(x,m,'.',label=r'Messwerte')
+plt.plot(m,x,'.',label=r'Messwerte')
 
-fitted ,a= curve_fit(func, x, m)
+fitted ,a= curve_fit(func, m, x)
 
 datei = open('build/text1.txt','w')
 datei.write(f'Die Kurve ist {fitted[0]:.4f}*m+({fitted[1]:.4f}). Damit ist k={9.81/fitted[0]:.4f}')
 #print(f'Die Kurve ist {fitted[0]:.4f}*m+({fitted[1]:.4f}). Damit ist k={9.81/fitted[0]:.4f}')
-x_new=np.linspace(1,5,50)
+x_new=np.linspace(1,8,50)
 fitted_curve = func(x_new, fitted[0], fitted[1])
 
 
 plt.plot(x_new,fitted_curve,label=r'Ausgleichsgerade')
 plt.legend(loc='best')
-plt.xlim(1.4,4.6)
+plt.xlim(1.8,7.1)
 
 with plt.xkcd():
-   plt.xlabel('x/cm')
-   plt.ylabel('m/g')
+   plt.ylabel('x/cm')
+   plt.xlabel('m/g')
 
 plt.savefig('build/Graph1.pdf')
 

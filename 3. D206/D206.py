c98cc3f077f94049ac8c27cb315d67b731bf2177
Niklas Düser <niklas.dueser@tu-dortmund.de>
2020-10-29 14:19:06 +0100

    Update

--- 3. D206/D206.py
+++ 3. D206/D206.py
@@ -13,11 +13,13 @@ t= t*60
 T1=Data[36:72]
 #print(T1)
 p1=Data[72:108]
+p1+=1
 #print(p1)
 T2=Data[108:144]
-#print(T2)
+print(T2)
 p2=Data[144:180]
-#print(p2)
+p2+=1
+print(p2)
 N=Data[180:216]
 #print(N)
 
@@ -73,13 +75,47 @@ for i in range(1,5):
      "videal(",7*i,")=",T1[(7*i)-1]/(T1[(7*i)-1]-T2[(7*i)-1]))
     print(N[7*i-1])
 
+        #Verdampfungswärme
+
+p0=p1[0]
+P1=np.log(p1/p0)
+parameter3, _ = np.polyfit(1/T1,P1, deg=1, cov=True)
+L1=-1*parameter3[0]*8.314
+print("L1=",L1)
+
+P2=np.log(p2/p0)
+parameter4, _ = np.polyfit(1/T2[5:30],P2[5:30], deg=1, cov=True)
+L2=-1*parameter4[0]*8.314
+print("L2=",L2)
+
+        #Massendurchsatz
+mdt=((mkck+m1c1)*Fdt(7*i))/L1
+print("Massendurchsatz= ",mdt)
+
+        #mechanische Leistung
+def N_mech(k,pa,pb,roh,mdt):
+    return 1/(k-1)*(pb*np.sqrt(pa/pb)-pa)*(1/roh)*mdt
+
+for i in range(1,5):
+    print(f"Die mechanische Leistung zu Minute{7*i} beträgt:{N_mech(1.14, p2[7*i -1], p1[7*i -1], 5.51, 10)}")
         #Plotten
 x= np.linspace(0,2100,50)
+x2=np.linspace(0.02,0.05,50)
+x3=np.linspace(0.05,0.35,50)
+plt.subplot(2,1,1)
 plt.plot(t,T1,"k.",label="T1")
 plt.plot(t,T2,".",label="T2") 
 plt.plot(x,f(x, *parameter1),label="Fit zu T1")
 plt.plot(x,g(x, *parameter2),label="Fit zu T2")     
 
+#plt.legend()
+#plt.savefig("build/plot.pdf")
+plt.subplot(2,1,2)
+plt.plot(1/T1,P1,".",label="Daten1")
+plt.plot(x2,parameter3[0]*x2+parameter3[1],label="regression1")
+plt.plot(1/T2[5:30],P2[5:30],".",label="Daten2")
+plt.plot(x3,parameter4[0]*x2+parameter4[1],label="regression2")
 plt.legend()
 plt.savefig("build/plot.pdf")
+
 #plt.show()

73afd2ee598a798791d57c0f49a08fffaa1c2a68
benedikt <benedikt.sander@tu-dortmund.de>
2020-11-08 00:42:49 +0100

    wenn es am ende klappt ist 1bar fertig

--- 4. V203/python/1bar.py
+++ 4. V203/python/1bar.py
@@ -4,6 +4,7 @@ import uncertainties as unc
 import uncertainties.unumpy as unp
 from uncertainties import ufloat
 from scipy.optimize import curve_fit
+import scipy.constants as const
 import sympy
 
 
@@ -16,20 +17,31 @@ T = np.load('python/variables/T_2.npy')
 
 umr=np.linspace(1,1,T.size)*273.15
 T = T + umr
+
 #Druck in pascal
 p = p*1e5
-print(np.log(p))
-print(1/T)
+#print(np.log(p))
+#print(1/T)
 
 #Ausgleichsrechnung für L
 #Verdampfungswärme L[Joule/mol] R[Joule/mol*K]
 
-#param, err = np.polyfit(1/T,np.log(p), deg=1, cov=True)
-#L1=-1*parameter3[0]*8.314
-#print("'\n'Param=",param)
-#print("err=",err)
-#print(f"L1={L1:.5f},'\n'")
+param, err = np.polyfit(1/T,np.log(p), deg=1, cov=True)
+L1=-1*param[0]*const.molar_gas_constant
+print("'\n'Param=",param)
+print("err=",err)
+print(f"L1={L1:.5f},'\n'")
 
+uparam=unp.uarray(param,err)
+uL1=-1*uparam[0]*const.molar_gas_constant
+
+La=const.molar_gas_constant * 373
+uLa=unc.ufloat(La,0)
+uLi=uL1-uLa
+uLimolekül=Li/(const.avogadro_constant*atomic_unit_of_charge)
+
+print('Werte für 1 bar')
+print(f'\n L1= {uL1} J/mol \n La={uLa}J/mol \n Li= {uLi} J/mol\n Li pro Molekül in eV: {uLimolekül} eV')
 
 
 
@@ -44,11 +56,14 @@ x1=np.linspace(0,1/T[0],1000)
 
 plt.figure()
 plt.plot(1/T,np.log(p),".",label="Werte von T")
-#plt.plot(x1, param[0]*x1 + param[1], label="Regression zu T")
-#plt.plot(1/T2,P2,".",label="Werte von T2")
-#plt.plot(x3, parameter4[0]*x3 + parameter4[1], label="Regression zu 2")
+plt.plot(x1, param[0]*x1 + param[1], label="Regression zu T")
 plt.xlabel("1/T [1/K]")
 plt.ylabel("log(p/p0)")
 plt.tight_layout()
 plt.legend()
 plt.savefig("build/plot1.pdf")
+
+
+
+#Variablen speichern
+np.save('python/variables/L1.npy', L1, allow_pickle=False)
--- 4. V203/python/einlesen.py
+++ 4. V203/python/einlesen.py
@@ -8,16 +8,36 @@ import sympy
 
 #1-15 bar
 p_1=np.linspace(1,15,29)
-np.save('variables/p_1.npy', p_1, allow_pickle=False)
+np.save('python/variables/p_1.npy', p_1, allow_pickle=False)
 
-T_1=np.array([1])
-np.save('variables/T_1.npy', T_1, allow_pickle=False)
+#T_1=np.array([1])
+T_1=np.linspace(1,15,29)
+np.save('python/variables/T_1.npy', T_1, allow_pickle=False)
 
 
 
 #bis 1bar
 T_2=np.array([5,5])
-np.save('variables/T_2.npy', T_2, allow_pickle=False)
+np.save('python/variables/T_2.npy', T_2, allow_pickle=False)
 
 p_2=np.array([1,63])
-np.save('variables/p_2.npy', p_2, allow_pickle=False)
+np.save('python/variables/p_2.npy', p_2, allow_pickle=False)
+
+
+#Dateien für Messwerttabelle
+np.savetxt(
+    'Mess1bar.txt',
+    np.column_stack([p_2, T_2]),
+    fmt=['%.5f', '%.5f'],       
+    delimiter=' & ',
+    header='p_2,T_2',
+)
+
+
+np.savetxt(
+    'Mess15bar.txt',
+    np.column_stack([p_1, T_1]),
+    fmt=['%.5f', '%.5f'],       
+    delimiter=' & ',
+    header='p_1,T_1',
+)

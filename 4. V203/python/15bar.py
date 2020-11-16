b5471d626fddc718441634cf257f06a69ce9833b
benedikt <benedikt.sander@tu-dortmund.de>
2020-11-16 22:39:56 +0100

    15bar korrektur

--- 4. V203/python/15bar.py
+++ 4. V203/python/15bar.py
@@ -56,8 +56,8 @@ def L2(x,a,b,c,d):
 
 
 #Theoriewerte
-xtheo=np.array([80,100,120,140,160,180,200])
-ytheo=np.array([41.585,40.657,39.684,38.643,37.518,36.304,34.962])
+xtheo=np.array([120,140,160,180,200])
+ytheo=np.array([39.684,38.643,37.518,36.304,34.962])
 
 umr=np.linspace(1,1,xtheo.size)*273.15
 xtheo= xtheo + umr
@@ -70,20 +70,6 @@ ytheo =ytheo *1000
 
 
 x= np.linspace(T[0],T[-1],1000)
-print(L1(T, *param))
-def test1(x,a,b,c,d):
-    return (np.sqrt((R*x/2)**2-C*(a*x**3 + b*x**2+c*x+d)))
-
-def test2(x,a,b,c,d):
-    return ((R*x/2))
-
-def test3(x,a,b,c,d):
-    return ((3*a*x**3+2*b*x**2+c*x)/(a*x**3+b*x**2+c*x+d))
-
-print("\n\n",test2(T, *param),"\n\n",test1(T, *param),"\n\ntemp: ",T[0:3],T[-1],"\nRT/2: ",test2(T[0:3], *param),"\nwurzel: ",test1(T[0:3], *param),"\npoly: ",test3(T[0:3], *param))
-print(T)
-
-
 
 
 plt.figure()

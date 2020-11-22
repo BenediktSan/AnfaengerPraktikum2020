7ccf9604f081f9a73ad05832f3ee2da8d3c20146
Niklas Düser <niklas.dueser@tu-dortmund.de>
2020-11-22 20:44:37 +0100

    fuck me

---
+++ 5. V311/python/c).py
@@ -0,0 +1,52 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import uncertainties as unc
+import uncertainties.unumpy as unp
+from uncertainties import ufloat
+from scipy.optimize import curve_fit
+import scipy.constants as const
+import sympy
+
+#       a)
+#elektrischer Widerstand R:
+R =
+
+#Spannungsabfall U_a:
+U_a=
+
+#geometrische Groeßen:
+
+
+#       b)
+#Hall-Effekt
+#Messungen U-+:
+Up=
+Um=
+
+#Hall Spannung
+U_h=
+
+
+#       c)
+# zu berechnen:
+# Ladungsträger pro Volumen n
+n=
+
+# Zahl der Ladungsträger pro Atom z
+z=
+
+# mittlere Flugzeit tau 
+tau=
+
+# mittlere Driftgeschwindigkeit vd
+j=1
+vd= 
+
+#Beweglichkeit mue:
+mue=
+
+#Totalgeschwindigkeit v:
+v=
+
+#mittlere freie Weglänge l:
+l=

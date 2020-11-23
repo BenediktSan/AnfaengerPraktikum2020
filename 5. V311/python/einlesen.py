0c0c4ad9c2a55dc6eecc3ea144f236ee23f77cb8
Niklas Düser <niklas.dueser@tu-dortmund.de>
2020-11-23 23:43:28 +0100

    test

---
+++ 5. V311/python/einlesen.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import uncertainties as unc
+import uncertainties.unumpy as unp
+from uncertainties import ufloat
+from scipy.optimize import curve_fit
+import sympy

e56f763243e7835536bee7939b5e30e878f84c92
benedikt <benedikt.sander@tu-dortmund.de>
2021-01-21 20:50:41 +0100

    .py basically fertig

--- Versuche Semester 3/10. V702/python/V702.py
+++ Versuche Semester 3/10. V702/python/V702.py
@@ -49,7 +49,8 @@ print(f'\nFit1:\nmu= {uparams1[0]:.5f} 1/s \nA0={uparams1[1]:.5f}\n')
 #Haltbwertszeit
 
 TV=(np.log(2))/uparams1[0]
-print(f'\nHalbwertszeit Vanadium= {TV:.5f} s \n')
+theoV=224.58
+print(f'\nHalbwertszeit Vanadium= {TV:.5f} s\tTheoriewert: {(theoV)} s  \t relative Abweichung: {((theoV-TV)/theoV)*100} % \n')
 
 #Halbwertszeit genauer
 
@@ -59,7 +60,7 @@ uparams1_=unp.uarray(params1_,err1_)
 print(f'\nFit1 genauer:\nmu= {uparams1_[0]:.5f} 1/s \nA0={uparams1_[1]:.5f}\n')
 
 TV_=(np.log(2))/uparams1_[0]
-print(f'\nHalbwertszeit Vanadium genauer= {TV_:.5f} s \n')
+print(f'\nHalbwertszeit Vanadium genauer= {TV_:.5f} s \tTheoriewert: {(theoV)} s  \t relative Abweichung: {((theoV-TV_)/theoV)*100} % \n')
 
 
 
@@ -80,20 +81,32 @@ uparams2=unp.uarray(params2,err2)
 print(f'\nFit2:\nmu= {uparams2[0]:.5f} 1/s \nA0={uparams2[1]:.5f}\n')
 
 TR=(np.log(2))/uparams2[0]
-print(f'\nHalbwertszeit Rhodium 104= {TR:.5f} s \n')
+theoR=4.34*60
+print(f'\nHalbwertszeit Rhodium 104= {TR:.5f} s \tTheoriewert: {(theoR)} s  \trelative Abweichung: {((theoR-TR)/theoR)*100} %  \n')
 
 #kurzlebiges Isotop
+x=9    #parameter zum rumprobieren(weite des schätzabschnittes)
+y=13
+
+Nr_kurz=Nr[0:x]-uparams2[1]*unp.exp(uparams2[0]*tr[0:x])
+
+params3, _3 =curve_fit(huell,tr[0:x],unp.nominal_values(Nr_kurz),p0=(0.0016,800))
+err3 =np.sqrt(np.diag(_3))
+uparams3=unp.uarray(params3,err3)
+print(f'\nFit3:\nmu= {uparams3[0]:.5f} 1/s \nA0={uparams3[1]:.5f}\n')
 
 
-
-
-
+TR2=(np.log(2))/uparams3[0]
+theoR2=42.3
+print(f'\nHalbwertszeit Rhodium 104i= {TR2:.5f} s\tTheoriewert: {(theoR2)} s  \trelative Abweichung: {((theoR2-TR2)/theoR2)*100} % \n')
 
 #PLOTTEN
 
 #plotparameter
 höhe=np.linspace(1,6,100)
 tcut=np.linspace(250,250,100)
+
+
 
 #plots
 
@@ -116,8 +129,9 @@ plt.savefig("build/plots/plot1.pdf")
 #Rhodium
 plt.figure()
 plt.errorbar(tr,unp.nominal_values(unp.log(Nr_)),yerr=unp.std_devs(unp.log(Nr_)),fmt='.',label="Messwerte")
-#plt.plot(tr,np.log(Nr),"x",label="Messwerte")
 plt.plot(tr,np.log(huell(tr,*params2)),label=r"Fit-Funktion für 104 Rh")
+plt.plot(tr[0:(x+y)],np.log(huell(tr[0:(x+y)],*params3)),'m',label=r"Fit-Funktion für 104i Rh")
+plt.plot(tr,np.log(huell(tr,*params3)+huell(tr,*params2)),label=r"Kombination beider Fits")
 plt.plot(tcut,höhe,"g--",linewidth=0.6,label=r"t'")
 plt.ylabel(r"$ln(N_V-N_0)$")
 plt.xlabel(r"$t/[s]$")
@@ -125,7 +139,14 @@ plt.tight_layout()
 plt.legend()
 plt.savefig("build/plots/plot2.pdf")
 
-
+plt.figure()
+plt.errorbar(tr[0:x],unp.nominal_values(unp.log(Nr_kurz)),yerr= unp.std_devs(unp.log(Nr_kurz)),fmt='.',label="Messwerte")
+plt.plot(tr[0:(x)],np.log(huell(tr[0:(x)],*params3)),'m',label=r"Fit-Funktion für 104i Rh")
+plt.ylabel(r"$ln(N_V-N_0)$")
+plt.xlabel(r"$t/[s]$")
+plt.tight_layout()
+plt.legend()
+plt.savefig("build/plots/plot3.pdf")
 #WERTE SPEICHERN
 
 np.savetxt(
@@ -150,5 +171,21 @@ np.savetxt(
     np.column_stack([tr,Nr,unp.nominal_values(unp.log(Nr_)), unp.std_devs(unp.log(Nr_))]),
     fmt='%-20s', 
     delimiter=' & ',
-    header='t,Nr,Nv_,err',
+    header='t,Nr,Nr_,err',
+)
+
+np.savetxt(
+    'build/tab/Rhodium_ln.txt',
+    np.column_stack([tr,unp.nominal_values(unp.log(Nr_)), unp.std_devs(unp.log(Nr_))]),
+    fmt='%-20s', 
+    delimiter=' & ',
+    header='t,log(Nr_),log(err)',
+)
+
+np.savetxt(
+    'build/tab/Nr_kurz.txt',
+    np.column_stack([tr[0:x],unp.nominal_values(Nr_kurz),unp.std_devs(Nr_kurz) ,unp.nominal_values(unp.log(Nr_kurz)), unp.std_devs(unp.log(Nr_kurz))]),
+    fmt='%1.3f', 
+    delimiter=' & ',
+    header='t,Nr,Nr_,err,ln(Nr),ln(err)',
 )

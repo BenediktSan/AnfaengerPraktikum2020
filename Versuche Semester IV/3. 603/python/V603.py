cfb79fac291ecb1e2e0c4a51825f484595a69bd0
Niklas Düser <niklas.dueser@tu-dortmund.de>
2021-05-02 13:58:06 +0200

    Progress

--- Versuche Semester IV/3. 603/python/V603.py
+++ Versuche Semester IV/3. 603/python/V603.py
@@ -55,24 +55,19 @@ theta_C , N_C = np.genfromtxt("EmissionCu.dat", unpack = True)
 #print(N_C)
 
 #Emission = zip(theta_C, N_C)
-
 #for s in len(N_C):
 #    N_C[s] = int(N_C[s])
 #list = []
-
 #for i in range(len(theta_C)):
 #    for n in range(N_C[i]):
 #        list.append(theta_C[i])
-
 #print(list)
-
-
 #E_l= list(Emission)
 #print(len(E_l))
-
-#print(Emission.theta_C)
-plt.figure()
 #plt.hist(E_l, bins = 40)
+#print(Emission.theta_C)
+
+plt.figure()
 plt.plot(theta_C, N_C , "y.", label = "Daten Cu")
 plt.legend()
 plt.xlabel("Einfallswinkel in °")
@@ -94,13 +89,13 @@ N_A_err = np.sqrt(N_A_)
 N_0 = unp.uarray(N_0_ , N_0_err)
 N_A = unp.uarray(N_A_ , N_A_err)
 
-
 lam = 2 * d_LiF * np.sin(theta_0 * np.pi / 180)
 
 # Korrektur der Totzeit
 
 N0 = N_0 / (1 - tau*N_0)
 NA = N_A / (1 - tau*N_A)
+#print(N_0 / N0)
 T = NA / N0
 
 params, cov = np.polyfit(lam, unp.nominal_values(T), deg=1, cov=True)
@@ -122,6 +117,7 @@ plt.ylabel(r'Transmission')
 plt.savefig('build/plots/transmission.pdf')
 
 I_0 = 2731.0
+print("I_0",(I_0-(I_0 / (1 - tau*(I_0/300)))/I_0))
 I_1 = 1180.0
 I_2 = 1024.0
 
@@ -133,14 +129,15 @@ print(f'T_1 = {T_1:.3f}, T_2 = {T_2:.3f}')
 
 a = unp.uarray(params[0], errs[0])
 b = unp.uarray(params[1], errs[1])
+
 lam_1 = (T_1 - b)/a
 lam_2 = (T_2 - b)/a
 
 lam_c = lam_2 - lam_1
 
-print(f'Lambda 1 = {lam_1}')
-print(f'Lambda 2 = {lam_2}')
-print(f'Compton = {lam_c}')
+print(f'Lambda 1 = {lam_1.n , lam_1.s}')
+print(f'Lambda 2 = {lam_2.n , lam_2.s}')
+print(f'Compton  = {lam_c.n , lam_c.s}')
 
 
 #Diskussion
@@ -148,9 +145,12 @@ print(f'Compton = {lam_c}')
 E_a_t = 8048.11
 E_b_t = 8906.9
 
-ab_a = (E_a_t - E_a) * 100 / E_a_t
-ab_b = (E_b_t - E_b) * 100 / E_b_t
+print("Energie Alpha", E_ae.n, E_ae.s)
+print("Energie Beta", E_be.n, E_be.s)
+ab_a = (E_a_t - E_ae) * 100 / E_a_t
+ab_b = (E_b_t - E_be) * 100 / E_b_t
 
-#print(f'Abweichung der Abweichung ')
+print(f'Abweichung der alpha Linie ', ab_a)
+print(f'Abweichung der beta Linie ', ab_b)
 
 

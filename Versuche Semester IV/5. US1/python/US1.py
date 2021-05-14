8eaecd62db15035ac32f19981f21adebcd40d529
Niklas Düser <niklas.dueser@tu-dortmund.de>
2021-05-14 17:33:51 +0200

    prep

---
+++ Versuche Semester IV/5. US1/python/US1.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import uncertainties as unc
+import uncertainties.unumpy as unp
+from uncertainties import ufloat
+from scipy.optimize import curve_fit
+import scipy.constants as const
+import sympy
+import os
+from tabulate import tabulate
+
+
+

4a5c2881618d486f8743e0af7020bb3ec16d5a67
benedikt <benedikt.sander@tu-dortmund.de>
2021-05-25 00:54:05 +0200

    big uff, i tell ya

--- Versuche Semester IV/6. 601/python/V601.py
+++ Versuche Semester IV/6. 601/python/V601.py
@@ -18,21 +18,41 @@ if os.path.exists("build/plots") == False:
 
 #####MEsSWERTE#########
 
-del1=1
-U1=1
 
-del2=1
-del2=1
+U1=np.linspace(1,22,22)
+U1[-4]=U1[-5]+0.5
+U1[-3]=U1[-4]+0.5
+U1[-2]=U1[-1]+1
+U1[-1]=U1[-2]+1
+
+
+
+
+del1=np.array([0.2,0.15,0.20,0.25,0.2,0.2,0.30,0.3,0.4,0.4,0.65,0.7,0.65,0.85,1.1,1.5,1.8,2.7,1.6,0.7,0.5,0.05])
+
+
+#del2=1/np.array([0.6,1.45,1.35,1.35,1.3,1.15,1.05,0.75,0.25,0.2,0.2])
+
+U2=np.linspace(0.5,21.5,22)-1
+U2[0]=0
+U2[-4]=U2[-4]+0.5
+U2[-3]=U2[-3]+0.5
+U2[-2]=U2[-2]+1
+
+
+
+del2=np.array([1.2,1.45,1.35,1.35,1.3,1.15,1.05,0.75,0.25,0.2,0.20,0,0,0,0,0.05,0.05,0,0,0,0.05,0.05])
+
 
 
 
 
 ####mittlere freie weglänge#####
 
-T=1
-p=(5.5*10^7*np.exp(-6876/T))e-3 #in bra
-omega=(0.0029/p)e-2 #in meter
-a=10^-2             #in meter
+T=np.array([20.5,148,182]) +273.15
+p=(5.5*10**7*np.exp(-6876/T)) #in bra
+omega=(0.0029/p) #in meter
+a=1           #in meter
 
 print("\n\n######Mittlere freie Weglänge#####\n")
 table1 ={ 'T':T, 'omeag':omega,'verhältnis':a/omega }
@@ -42,12 +62,45 @@ print("\n ",tabulate (table1, tablefmt="latex"))
 #####differentiele Energieverteilung######
 print("\n\n######Differentielle Energieverteilung#####\n")
 
+l1=1/np.array([2.8,2.75,2.4,2.2,2.6,2.4,2.4,2.2,2.7,2.75])
+print("Messung 1:")
+print(f'Spannung  {np.mean(l1):.2f} \pm {np.std(l1):.2f}')
+print("Kontaktpotential: ",11-6.6 )
+
+U1=U2*np.mean(l1)
+U2=U2*np.mean(l1)
 
 
+table1 ={ 'T':U1, 'omeag':del1,'U2':U2, 'del2':del2 }
+print("\n ",tabulate (table1, tablefmt="latex"))
 
 
+#####FRanck_hErtzs######
+
+l2=1/(np.array([5,4.6,4.6,4.9])/10)
 
 
+print("\n\n######Franck hertzs#####\n")
+print(f'Spannung {np.mean(l2):.2f} \pm {np.std(l2):.2f}')
+
+
+d=np.array([2.4,2.4,2.6,2.5,2.7,2.6,3])
+d_=d*np.mean(l2)
+
+print(f'mean distance {np.mean(d_):.2f} \pm {np.std(d_):.2f}')
+mean_=ufloat(np.mean(d_),np.std(d_))
+print(mean_)
+print(f' lamda: {(const.c/((mean_*const.elementary_charge)/const.h))*10**9} nm')
+lam=(const.c/((mean_*const.elementary_charge)/const.h))
+e=mean_*const.elementary_charge
+
+theo=(const.c/(10.438*const.elementary_charge/const.h))
+print("lambda theo: ", theo*10**9)
+print(f'relabw Energy: {(10.438-mean_)/10.438}percent \n relabw lamda: {((theo-lam)/theo)}percent')
+
+x=np.linspace(1,7,7)
+table1 ={ 'T':x, 'omeag':d,'del2':d_ }
+print("\n ",tabulate (table1, tablefmt="latex"))
 
 
 
@@ -68,7 +121,9 @@ print("\n\n######Differentielle Energieverteilung#####\n")
 
 
 #########PLOTS#########
-
+a=6.6
+x=np.linspace(a,a,100)
+y=np.linspace(0,2.7,100)
 
 #plt.figure()
 #plt.plot(t,np.log(U_),"x",label="Messwerte")
@@ -80,21 +135,24 @@ print("\n\n######Differentielle Energieverteilung#####\n")
 #plt.legend()
 #plt.savefig("build/plots/plot0.pdf")
 
-plt.plot(U1, del1, 'rx', label='Messdaten')
-plt.ylabel(r'\frac{\increment y}{\increment x} [EINHEIT]')
-plt.xlabel(r'$U_A$')
-plt.yscale('log')
+plt.figure()
+plt.plot(U1,del1, 'rx', label='Messdaten')
+plt.plot(x,y,"g--",linewidth=0.6)
+plt.ylabel(r'$\frac{\Delta y}{\Delta x} $')
+plt.xlabel(r'$U_A$ [V]')
+#plt.yscale('log')
 plt.legend(loc='best')
 plt.tight_layout()
-plt.savefig('plot1.pdf')
+plt.savefig('build/plots/plot1.pdf')
 
-plt.plot(U2, del2, 'rx', label='Messdaten')
-plt.ylabel(r'\frac{\increment y}{\increment x} [EINHEIT]')
-plt.xlabel(r'$U_A$')
-plt.yscale('log')
+plt.figure()
+plt.plot(U2,del2,  'rx', label='Messdaten')
+plt.ylabel(r'$\frac{\Delta y}{\Delta x} $')
+plt.xlabel(r'$U_A$ [V]')
+##plt.yscale('log')
 plt.legend(loc='best')
 plt.tight_layout()
-plt.savefig('plot2.pdf')
+plt.savefig('build/plots/plot2.pdf')
 
 
 

f989db248c0f3c4437a74776f0e52e1d5a84a519
Niklas Düser <niklas.dueser@tu-dortmund.de>
2021-06-17 14:23:08 +0200

    There you go bene

--- Versuche Semester IV/10. V302/python/einlesen.py
+++ Versuche Semester IV/10. V302/python/einlesen.py
@@ -8,6 +8,15 @@ if os.path.exists("python/variables") == False:
   
 ###  Vorgegebene Daten einlesen und einspeichern
 
+print("a) 1.", 332 * (542/ 458))
+print("a) 2.", 664 * (370/630))
 
-np.save('python/variables/I_aufsteigend.npy', I_aufsteigend, allow_pickle=False)
+print("b) 1. R_x : " , 205 * (580/420))
+print("b) 1. C_x : " , 750e-9 * (420/580))
 
+print("b) 2. R_x : " , 503 * (451/549))
+print("b) 2. C_x : " , 399e-9 * (548/452))
+
+
+
+print("f) 1. v_0 :" , 1/ (1000 * 295e-9 * 2 * np.pi))
